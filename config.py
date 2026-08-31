e51a7fa6d7d5b60abc5cd4aa5a57226a567b9820
KK9823 <Kaungkha0469@gmail.com>
2026-08-31 14:47:25 -0700

    ignored __pycache__ and updated some hyperparameters

--- config.py
+++ config.py
@@ -2,14 +2,14 @@
 vocab_size = 155            # This shouldn't be changed unless training data changes and more chars appear
 block_size = 128            # How many characters the model processes at one time
 d_model = 512               # Each character is turned into a d_model dimensional vector
-n_heads = 2                 # Number of heads
-n_layers = 8                # Amount of times the transformer block is run
+n_heads = 4                 # Number of heads
+n_layers = 4                # Amount of times the transformer block is run
 dropout = 0.1               # random dropout
 batch_size = 64             # batch size for training
 learning_rate = 6e-4        # how much gradients change the model in training
 
 # Training
-max_steps = 50_000            # How many steps in total the model will be trained
+max_steps = 5_000            # How many steps in total the model will be trained
 eval_interval = 200         # The model will be evaluated every eval_interval
 
 # Use "cuda" for Nvidia gpu, "mps" for Apple Silicon, or "cpu" for CPU
